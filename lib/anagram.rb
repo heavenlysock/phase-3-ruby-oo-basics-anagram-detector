4a844f1c9574ad26e1ac561a8c0bf711c1ad2027
bobertsworld <bobertsworld@gmail.com>
2023-01-16 23:11:12 -0800

    commit tree ea9f4c23ccaa65e241cbd7ce173caf661d4b302b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+
+    def intialize(word)
+        @name = word
+    end
+
+    # def 
+    # @name = name
+    # end
+
+    
+end
+
+
+
+# anagram=Anagram.new
+# anagram.name
